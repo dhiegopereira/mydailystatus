ea89232b2edfe46391fdf104dede755812b31ad0
dhiegopereira <33781733+dhiegopereira@users.noreply.github.com>
2020-06-08 22:53:48 -0300

    integration firestore

--- pages/app.js
+++ pages/app.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import auth0 from '../lib/auth0'
 import router from 'next/router'
+import { db }  from '../lib/db'
 
 const App = (props) => {
     useEffect(() => {
@@ -25,10 +26,25 @@ export async function getServerSideProps ({ req, res }) {
     const session = await auth0.getSession(req)
     console.log(session)
     if (session) {
+        const todaysCheckin = await db
+            .collection('markers')
+            .doc('2020-06-08')
+            .collection('checks')
+            .doc(session.user.sub)
+            .get()
+        
+        const todayData = todaysCheckin.data()
+        let forceCreate = true
+        if(todayData){
+
+            forceCreate = false
+        }
+        
         return {
             props: {
                 isAuth: true,
-                user: session.user
+                user: session.user,
+                forceCreate
             }
         }
     }
@@ -39,3 +55,6 @@ export async function getServerSideProps ({ req, res }) {
         }
     }
 }
+
+
+//31:45
