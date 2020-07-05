17691ce5eec1bdaa2ade25d1d10091ffb2600329
dhiegopereira <33781733+dhiegopereira@users.noreply.github.com>
2020-07-05 16:19:40 -0300

    continue development

--- pages/app.js
+++ pages/app.js
@@ -2,20 +2,54 @@ import React, { useEffect } from 'react'
 import auth0 from '../lib/auth0'
 import router from 'next/router'
 import { db }  from '../lib/db'
+import { distance } from '../lib/geo'
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 const App = (props) => {
     useEffect(() => {
         if(!props.isAuth){
             router.push('/')
+        } else if (props.forceCreate) {
+            router.push('/create-status')
         }
     })
-    if(!props.isAuth){
+    if(!props.isAuth || props.forceCreate){
         return null
     }
     return (
         <div>
-            <h1>App</h1>
-            <pre>{JSON.stringify(props, null, 2)}</pre>
+            <h1>Status próximos a você:</h1>
+            <table>
+                {props.checkins.map(checkin => {
+                    return (
+                        <tr>
+                            <td>{checkin.id === props.user.sub && 'Seu status'}</td>
+                            <td>{checkin.status}</td>
+                            <td>{JSON.stringify(checkin.coords)}</td>
+                            <td>{checkin.distance}</td>
+                        </tr>
+                    )
+                })}
+            </table> 
+            {props.checkins.map(checkin => {
+                return (
+                    <div style={{ height: '500px', width: '500px' }}>
+                        <GoogleMapReact
+                        bootstrapURLKeys={{ key: 'AIzaSyC7h-rXw0OKkcuaeGh8Q-hD64DjgJV4PWY' }}
+                        defaultCenter={checkin.center}
+                        defaultZoom={checkin.zoom}
+                        >
+                        <AnyReactComponent
+                            lat={checkin.coords.lat}
+                            lng={checkin.coords.long}
+                            text={checkin.status}
+                        />
+                        </GoogleMapReact>
+                    </div>
+                )
+            })}           
         </div>
     )
 }
@@ -24,11 +58,12 @@ export default App
 
 export async function getServerSideProps ({ req, res }) {
     const session = await auth0.getSession(req)
-    console.log(session)
+    const today = new Date()
+    const currentDate = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate()        
     if (session) {
         const todaysCheckin = await db
             .collection('markers')
-            .doc('2020-06-08')
+            .doc(currentDate)
             .collection('checks')
             .doc(session.user.sub)
             .get()
@@ -36,10 +71,46 @@ export async function getServerSideProps ({ req, res }) {
         const todayData = todaysCheckin.data()
         let forceCreate = true
         if(todayData){
-
-            forceCreate = false
-        }
-        
+            const checkins = await db
+            .collection('markers')
+            .doc(currentDate)
+            .collection('checks')
+            .near({
+                center: todayData.coordinates,
+                radius: 1000
+            })
+            .get()
+            const checkinsList = []
+            checkins.docs.forEach(doc => {
+                checkinsList.push({
+                    id: doc.id,
+                    status: doc.data().status,
+                    coords: {
+                        lat: doc.data().coordinates.latitude,
+                        long: doc.data().coordinates.longitude
+                    },
+                    distance: distance(
+                        -2.900360, //todayData.coordinates.latitude, 
+                        -40.842949, //todayData.coordinates.longitude, 
+                        doc.data().coordinates.latitude, 
+                        doc.data().coordinates.longitude
+                    ).toFixed(2),
+                    center: {
+                        lat: 59.95,
+                        lng: 30.33
+                    },
+                    zoom: 11
+                })
+            })
+            return {
+                props: {
+                    isAuth: true,
+                    user: session.user,
+                    forceCreate: false,
+                    checkins: checkinsList
+                }
+            }
+        }  
         return {
             props: {
                 isAuth: true,
@@ -57,4 +128,4 @@ export async function getServerSideProps ({ req, res }) {
 }
 
 
-//31:45
+//01:19:40
---
+++ pages/create-status.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import auth0 from '../lib/auth0'
+import axios from 'axios'
+
+const CreateStatus = () => {
+    const [dados, setDados] = useState({
+        status: 'bem',
+        coords: {
+            lat: null,
+            long: null
+        }
+    })
+
+    const getMyLocation = () => {
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(position => {
+                console.log(position.coords)
+                setDados(old => {
+                    return {
+                        ...old, 
+                        coords: {
+                            lat: position.coords.latitude,
+                            long: position.coords.longitude
+                    }
+                    }
+                })
+            })
+        }
+    }
+    const onStatusChange = evt => {
+        const value = evt.target.value
+        setDados(old => {
+            return {
+                ...old,
+                status: value
+            }
+        })
+    }
+    const save = async() => {
+        await axios.post('/api/save-status', dados)
+    }
+    return (
+        <div>
+            <h1>Create Status</h1>
+            <label className='block'>
+                <input onClick={onStatusChange} type='radio' name='status' value='bem'/>
+                Estou bem sem sitomas
+            </label>
+
+            <label className='block'>
+                <input onClick={onStatusChange} type='radio' name='status' value='gripe'/>
+                    Estou com sitomas de gripe / resfriado                 
+            </label>
+
+            <label className='block'>
+                <input onClick={onStatusChange} type='radio' name='status' value='covid'/>
+                    Estou com sitomas da covid-19                   
+            </label>
+            Sua posição atual: {JSON.stringify(dados)}
+            <button onClick={getMyLocation}>Pegar minha localização</button>
+            <button onClick={save}>Salvar meu status</button>
+        </div> 
+
+    )
+}
+        
+
+export default CreateStatus
+
+export async function getServerSideProps ({ req, res }) {
+    const session = await auth0.getSession(req)
+    console.log(session)
+    if (session) { 
+        return {
+            props: {
+                isAuth: true,
+                user: session.user
+            }
+        }
+    }
+    return {
+        props: {
+            isAuth: false,
+            user: {}
+        }
+    }
+}
+
